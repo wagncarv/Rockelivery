7cad9fdd877d045a876c749b4f694664674c1490
Wagner Carvalho <wagncarv@gmail.com>
2021-08-04 23:10:10 -0300

    show user

--- lib/test.ex
+++ lib/test.ex
@@ -1,19 +1,23 @@
 defmodule Test do
     alias Rockelivery.User
     alias  Rockelivery.Users.Create
+    alias Rockelivery.Users.Get
 
     def test do
-       param =  %{
-           age: 30,
-           address: "Rua AB",
-           cep: "29176195" ,
-           cpf: "12345678901",
-           email: "wagn@mail.com" ,
-           password: "123456",
-           name: "Wagner"
-        }
+        # "91712c47-2086-4b8b-85ea-155ac032c514"
+    #    param =  %{
+    #        age: 30,
+    #        address: "Rua AB",
+    #        cep: "29176195" ,
+    #        cpf: "12345678909",
+    #        email: "wagncarv@mail.com" ,
+    #        password: "123456",
+    #        name: "Wagner"
+    #     }
 
-        Create.call(param)
+        # Create.call(param)
+
+        Get.by_id("91712c47-2086-4b8b-85ea-155ac032c515")
     end
 
 
